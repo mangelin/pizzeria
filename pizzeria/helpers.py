735e0f071c09aeaed286368a93c36e609e2b6a81
Mauro Gilardi <galvao.m@gmail.com>
2021-02-21 11:22:54 +0000

    Adding calculate_score()

--- pizzeria/helpers.py
+++ pizzeria/helpers.py
@@ -3,3 +3,11 @@ def get_unique_ingredients(listOfPizzas, pizzas):
     for pizza_id in listOfPizzas:
         ingredients += pizzas[pizza_id]
     return set(sorted(ingredients))
+
+
+def calculate_score(pizzas_to_deliver, pizzas_menu):
+    score = 0
+    for pizza_ids in pizzas_to_deliver:
+        score += len(get_unique_ingredients(pizza_ids, pizzas_menu)) ** 2
+    return score
+
